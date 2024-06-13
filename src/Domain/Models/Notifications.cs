e960211fcde17b2ea97e93c051f542bbd7219c15
Igor Cordeiro <igorcordeiro@id.uff.br>
2024-06-13 16:25:18 -0300

    Corrigindo os Warnings da classe Notifications

--- src/Domain/Models/Notifications.cs
+++ src/Domain/Models/Notifications.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Models
+namespace Domain.Models;
+
+public class Notifications
 {
-    public class Notifications
-    {
-        public int? Id { get; set; }
-        public int SubscriptionId { get; set; }
-        public string Message { get; set; }
-        public bool Read { get; set; }
-    }
+    public int? Id { get; set; }
+    public int SubscriptionId { get; set; }
+    public string Message { get; set; } = "";
+    public bool Read { get; set; } = false;
 }
--- src/Domain/Models/Subscriptions.cs
+++ src/Domain/Models/Subscriptions.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Models
-{
-    public class Subscriptions
-    {
-        public int Id { get; set; }
-        public int SubTaskIdSubscriber { get; set; }
-        public int MainTaskIdTopic { get; set; }
+namespace Domain.Models;
 
-    }
+public class Subscriptions
+{
+    public int Id { get; set; }
+    public int SubTaskIdSubscriber { get; set; }
+    public int MainTaskIdTopic { get; set; }
+
 }
