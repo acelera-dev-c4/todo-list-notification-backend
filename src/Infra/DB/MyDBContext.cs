db693756deb93a11991ec5348453deb43fb6f710
Anderson Gomes <anderson.informatica2016@gmail.com>
2024-06-13 21:08:08 -0300

    Adicionados os modelos MainTask e SubTask para permitir especificar o relacionamento de chave estrangeira da Subscription no banco

--- src/Infra/DB/MyDBContext.cs
+++ src/Infra/DB/MyDBContext.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace Infra.DB
+public class MyDBContext : DbContext
 {
-	internal class MyDBContext
+	public DbSet<Notifications> Notifications { get; set; }
+	public DbSet<Subscriptions> Subscriptions { get; set; }
+
+	public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
+		modelBuilder.Entity<Notifications>()
+			.HasOne<Subscriptions>()
+			.WithMany()
+			.HasForeignKey(n => n.SubscriptionId)
+			.IsRequired();
+
+		modelBuilder.Entity<Subscriptions>(entity =>
+		{
+			entity.HasOne<MainTask>()
+				.WithMany()
+				.HasForeignKey(s => s.MainTaskIdTopic)
+				.IsRequired();
+
+			entity.HasOne<SubTask>()
+				.WithMany()
+				.HasForeignKey(s => s.SubTaskIdSubscriber)
+				.IsRequired();
+		});
+
+		base.OnModelCreating(modelBuilder);
 	}
+
 }
