bca968eb2fec3d6c7b297f0fe434fada1a88aceb
Vagner1212 <corvonegro222@gmail.com>
2024-06-14 16:41:50 -0300

    Criação da Middlewares Exception

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Api.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,6 +19,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware(typeof(ExceptionHandler));
 
 app.UseAuthorization();
 
